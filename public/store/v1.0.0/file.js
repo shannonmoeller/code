b5dec6da40a6511762291ce31ce390b22decb35b
Shannon Moeller <me@shannonmoeller.com>
2020-11-27 22:53:21 +0000

    add file and local stores, update dotfiles

---
+++ public/store/v1.0.0/file.js
@@ -0,0 +1,40 @@
+import { createStore } from './store.js';
+
+export function createFileStore(initialState, options = {}) {
+	const { encode = JSON.stringify, decode = JSON.parse, ...rest } = options;
+
+	let handle;
+
+	const store = {
+		...createStore(initialState),
+
+		async open(asNew = false) {
+			if (asNew || !handle) {
+				[handle] = await window.showOpenFilePicker({
+					multiple: false,
+					...rest,
+				});
+			}
+
+			const readable = await handle.getFile();
+
+			store.set(decode(await readable.text()));
+		},
+
+		async save(asNew = false) {
+			if (asNew || !handle) {
+				handle = await window.showSaveFilePicker({
+					excludeAcceptAllOption: true,
+					...rest,
+				});
+			}
+
+			const writable = await handle.createWritable();
+
+			await writable.write(encode(store.get()));
+			await writable.close();
+		},
+	};
+
+	return store;
+}
---
+++ public/store/v1.0.0/local.js
@@ -0,0 +1,27 @@
+import { createStore } from './store.js';
+
+function getStorage(key) {
+	try {
+		return JSON.parse(localStorage.getItem(key));
+	} catch (e) {
+		return null;
+	}
+}
+
+function setStorage(key, value) {
+	try {
+		localStorage.setItem(key, JSON.stringify(value));
+	} catch (e) {
+		return;
+	}
+}
+
+export function createLocalStore(key, initialState) {
+	const store = createStore(getStorage(key) ?? initialState);
+
+	store.subscribe((state) => {
+		setStorage(key, state);
+	});
+
+	return store;
+}
